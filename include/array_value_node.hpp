3819c5763d930c8b0087ac873d728a756e119012
Jasper Young <jasper.c.young@gmail.com>
2023-02-13 19:00:42 -0500

    Progress work for improving array node interface

--- include/array_value_node.hpp
+++ include/array_value_node.hpp
@@ -29,16 +29,14 @@ public:
   using const_reverse_iterator = vector_t::const_reverse_iterator;
 
 private:
-  value_t m_value;
+  vector_t m_contents;
 
 public:
   array_value_node();
-  explicit array_value_node(const value_t &value);
-  explicit array_value_node(value_t &&value);
   array_value_node(const array_value_node &other);
   array_value_node(array_value_node &&other);
 
-  virtual ~array_value_node();
+  virtual ~array_value_node() override;
 
 public:
   virtual actual_node_type get_actual_node_type() const override;
@@ -47,19 +45,93 @@ public:
   virtual array_value_node *create_clone() const override;
   virtual value_node_type get_value_node_type() const override final;
 
-  const value_t &get() const;
-  value_t &get();
-  void set(const value_t &value);
-  void set(value_t &&value);
+  reference at(size_type pos);
+  const_reference at(size_type pos) const;
+  reference operator[](size_type pos);
+  const_reference operator[](size_type pos) const;
+  reference front();
+  const_reference front() const;
+  reference back();
+  const_reference back() const;
+  value_type *data();
+  const value_type *data() const;
+
+  iterator begin();
+  const_iterator begin() const;
+  const_iterator cbegin() const;
+  iterator end();
+  const_iterator end() const;
+  const_iterator cend() const;
+  reverse_iterator rbegin();
+  const_reverse_iterator rbegin() const;
+  const_reverse_iterator crbegin() const;
+  reverse_iterator rend();
+  const_reverse_iterator rend() const;
+  const_reverse_iterator crend() const;
+
+  bool empty() const;
+  size_type size() const;
+  size_type max_size() const;
+  void reserve(size_type new_cap);
+  size_type capacity() const;
+  void shrink_to_fit();
+
+  void clear();
+  iterator insert(const_iterator pos, const value_type &value);
+  iterator insert(const_iterator pos, value_type &&value);
+  iterator insert(const_iterator pos, size_type count, const value_type &value);
+  template <typename InputIt>
+  iterator insert(const_iterator pos, InputIt first, InputIt last) {
+    return m_contents.insert(pos, first, last);
+  }
+  iterator insert(const_iterator pos, std::initializer_list<value_type> ilist);
+  template <typename... Args>
+  iterator emplace(const_iterator pos, Args &&...args) {
+    return m_contents.emplace(pos, std::move(args...));
+  }
+  iterator erase(const_iterator pos);
+  iterator erase(const_iterator first, const_iterator last);
+  void push_back(const value_type &value);
+  void push_back(value_type &&value);
+  template <typename... Args> reference emplace_back(Args &&...args) {
+    return m_contents.emplace_back(std::move(args...));
+  }
+  void pop_back();
+  void resize(size_type count);
+  void resize(size_type count, const value_type &value);
+  void swap(array_value_node &other);
 
 public:
   array_value_node &operator=(const array_value_node &other);
   array_value_node &operator=(array_value_node &&other);
-  array_value_node &operator=(const value_t &value);
-  array_value_node &operator=(value_t &&value);
 
-  explicit operator value_t() const;
+public:
+  friend bool operator==(const array_value_node &lhs,
+                         const array_value_node &rhs);
+  friend auto operator<=>(const array_value_node &lhs,
+                          const array_value_node &rhs)
+      -> decltype(lhs.m_contents <=> rhs.m_contents);
+  friend void swap(array_value_node &lhs, array_value_node &rhs);
+  template <typename U>
+  friend array_value_node::size_type erase(array_value_node &c, const U &value);
+  template <typename Pred>
+  friend array_value_node::size_type erase_if(array_value_node &c, Pred pred);
 };
+
+bool operator==(const array_value_node &lhs, const array_value_node &rhs);
+auto operator<=>(const array_value_node &lhs, const array_value_node &rhs)
+    -> decltype(lhs.m_contents <=> rhs.m_contents);
+void swap(array_value_node &lhs, array_value_node &rhs);
+template <typename U>
+array_value_node::size_type erase(array_value_node &c, const U &value) {
+  using std::erase;
+  return erase(c.m_contents, value);
+}
+template <typename Pred>
+array_value_node::size_type erase_if(array_value_node &c, Pred pred) {
+  using std::erase_if;
+  return erase_if(c, pred);
+}
 } // namespace libconfigfile
 
 #endif
