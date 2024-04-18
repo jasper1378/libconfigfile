15bbd31e9402847b294adb718547737136737848
Jasper Young <jasper.c.young@gmail.com>
2024-04-17 22:15:15 -0400

    Add misc func to convert node_type to str
    Might be useful in applications that want to print error messages...

---
+++ source/misc.cpp
@@ -0,0 +1,28 @@
+#include "misc.hpp"
+
+#include "node_types.hpp"
+
+#include <string>
+
+std::string libconfigfile::misc::node_ptr_type_to_str(const node_type type) {
+  switch (type) {
+  case ::libconfigfile::node_type::String: {
+    return "string";
+  } break;
+  case ::libconfigfile::node_type::Integer: {
+    return "integer";
+  } break;
+  case ::libconfigfile::node_type::Float: {
+    return "float";
+  } break;
+  case ::libconfigfile::node_type::Array: {
+    return "array";
+  } break;
+  case ::libconfigfile::node_type::Map: {
+    return "map";
+  } break;
+  default: {
+    return "null";
+  } break;
+  }
+}
